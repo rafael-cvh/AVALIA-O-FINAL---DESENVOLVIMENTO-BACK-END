816dfdace363eca41cda2806f2eae38caf12ec24
Alexandre <martinssilvaa38@gmail.com>
2026-06-27 18:41:07 -0300

    models de categoria, produto e user

--- avaliacao-final/app/models/categoria.ts
+++ avaliacao-final/app/models/categoria.ts
@@ -1,3 +1,9 @@
 import { CategoriaSchema } from '#database/schema'
+import { hasMany } from '@adonisjs/lucid/orm'
+import Produto from './produto.ts'
+import { type HasMany } from '@adonisjs/lucid/types/relations'
 
-export default class Categoria extends CategoriaSchema {}
+export default class Categoria extends CategoriaSchema {
+  @hasMany(() => Produto)
+  declare produtos: HasMany<typeof Produto>
+}
--- avaliacao-final/app/models/produto.ts
+++ avaliacao-final/app/models/produto.ts
@@ -1,3 +1,9 @@
 import { ProdutoSchema } from '#database/schema'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import Categoria from './categoria.ts'
+import { type BelongsTo } from '@adonisjs/lucid/types/relations'
 
-export default class Produto extends ProdutoSchema {}
+export default class Produto extends ProdutoSchema {
+  @belongsTo(() => Categoria)
+  declare categoria: BelongsTo<typeof Categoria>
+}
